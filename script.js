73715c1ca51230b795668d102753bd271f8eea3d
jrenzzo <u202118568@upc.edu.pe>
2022-02-21 18:05:18 -0500

    created gameboard

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const generalGameboard = {
-    gameboard: []
+const gameboard = (()=>{
+    let gameboard = []
+    return {gameboard}
+})();
+
+
+
+//factory functions
+const player = (name, figure)=>{
+    return {name, figure}
 }
 
-const FirstPlayer = {}
-const SecondPlayer = {}
+let player1
+let player2
+
+const boxes = document.querySelectorAll('.box')
+
+
+for(let i=0; boxes.length; i++){
+    boxes[i].addEventListener('click', ()=>{
+        boxes[i].setAttribute("style", "background-color: red;")
+    })
+}
